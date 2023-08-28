5e8d547679f55fe227c46ada00923cc328a3b85d
Darren Li <tm455340@gmail.com>
2023-08-29 00:38:42 +0800

    fix image not found

--- src/components/Movies/MovieCard/index.jsx
+++ src/components/Movies/MovieCard/index.jsx
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import style from './MovieCard.module.scss';
+import PhotoNotFound from '../../../assets/photo-not-found.svg';
 
 const MovieCard = (props) => {
   const {
     posterPath, title, overview, id,
   } = props;
-  const imageUrl = posterPath ? `https://image.tmdb.org/t/p/original${posterPath}` : '../../../../public/photo-not-found.svg';
+  const imageUrl = posterPath ? `https://image.tmdb.org/t/p/original${posterPath}` : PhotoNotFound;
   return (
     <Link to={`/movie/${id}`}>
       <ImageListItem className={style.movieCard}>
